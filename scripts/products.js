067762a57ca3e193e6cad0a9c1d587035c3e24dc
chawkii <achouriichawkii@gmail.com>
2023-12-09 15:37:42 +0100

    slider parfait

--- scripts/products.js
+++ scripts/products.js
@@ -1,3 +1,5 @@
+let selectedProducts;
+
 // format price fucntion
 function formatPrice(price) {
   let formattedPrice = (price / 100).toLocaleString('en-US', {
@@ -15,97 +17,97 @@ function formatPrice(price) {
 // products list
 const products1 = [{
   image: '/images/products/pic-1.png',
-  name: 'produit 01',
+  name: 'produit01',
   price: 1009090
 },{
   image: '/images/products/pic-1.png',
-  name: 'produit 02',
+  name: 'produit02',
   price: 2009525
 },{
   image: '/images/products/pic-1.png',
-  name: 'produit 03',
+  name: 'produit03',
   price: 7990000
 },{
   image: '/images/products/pic-1.png',
-  name: 'produit 04',
+  name: 'produit04',
   price: 20919500
 }];
 
 const products2 = [{
   image: '/images/products/pic-2.png',
-  name: 'produit 01',
+  name: 'produit01',
   price: 1009090
 },{
   image: '/images/products/pic-2.png',
-  name: 'produit 02',
+  name: 'produit02',
   price: 2009525
 },{
   image: '/images/products/pic-2.png',
-  name: 'produit 03',
+  name: 'produit03',
   price: 7990000
 },{
   image: '/images/products/pic-2.png',
-  name: 'produit 04',
+  name: 'produit04',
   price: 20919500
 }];
 
 const products3 = [{
   image: '/images/products/pic-3.png',
-  name: 'produit 01',
+  name: 'produit01',
   price: 1009090
 },{
   image: '/images/products/pic-3.png',
-  name: 'produit 02',
+  name: 'produit02',
   price: 2009525
 },{
   image: '/images/products/pic-3.png',
-  name: 'produit 03',
+  name: 'produit03',
   price: 7990000
 },{
   image: '/images/products/pic-3.png',
-  name: 'produit 04',
+  name: 'produit04',
   price: 20919500
 }];
 
 const products4 = [{
   image: '/images/products/pic-4.png',
-  name: 'produit 01',
+  name: 'produit01',
   price: 1009090
 },{
   image: '/images/products/pic-4.png',
-  name: 'produit 02',
+  name: 'produit02',
   price: 2009525
 },{
   image: '/images/products/pic-4.png',
-  name: 'produit 03',
+  name: 'produit03',
   price: 7990000
 },{
   image: '/images/products/pic-4.png',
-  name: 'produit 04',
+  name: 'produit04',
   price: 20919500
 }];
 
 const products5 = [{
   image: '/images/products/pic-1.png',
-  name: 'produit 01',
+  name: 'produit01',
   price: 1009090
 },{
   image: '/images/products/pic-2.png',
-  name: 'produit 02',
+  name: 'produit02',
   price: 2009525
 },{
   image: '/images/products/pic-3.png',
-  name: 'produit 03',
+  name: 'produit03',
   price: 7990000
 },{
   image: '/images/products/pic-4.png',
-  name: 'produit 04',
+  name: 'produit04',
   price: 20919500
 }];
 
 
 // generate the products in the html page
-function generateProductHTML(products) {
+function generateProductHTML(products, index) {
   let productsHTML = '';
   products.forEach((product) => {
     productsHTML += `
@@ -113,14 +115,14 @@ function generateProductHTML(products) {
     <div class="product-image-container">
       <img class="product-image" src="${product.image}">
     </div>
-    <div class="product-name limit-text-to-2-lines">
+    <div class="product-name">
       ${product.name}
     </div>
     <div class="product-price">
       ${formatPrice(product.price)} DZD
     </div>
     <div class="product-quantity-container">
-      <select>
+      <select class="js-quantity-selector" data-product-name="${product.name}">
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -137,7 +139,7 @@ function generateProductHTML(products) {
       <img src="images/icons/checkmark.png">
       Added
     </div>
-    <button class="add-to-cart-button button-primary js-add-to-cart"
+    <button class="add-to-cart-button js-add-to-cart"
     data-product-name="${product.name}">
       Add to Cart
     </button>
@@ -153,6 +155,7 @@ function generateProductHTML(products) {
 // slider function
 const radioInputs = document.querySelectorAll('input[name="slider"]');
 const productsGrid = document.querySelector('.js-products-grid');
+const indicators = document.querySelectorAll('.slider-indicator');
 
 function generateAndDisplayProducts(index) {
   let selectedProducts;
@@ -167,24 +170,18 @@ function generateAndDisplayProducts(index) {
   } else if (index === 4) {
     selectedProducts = products5;
   }
-  const html = generateProductHTML(selectedProducts);
+  const html = generateProductHTML(selectedProducts, index);
   productsGrid.innerHTML = html;
-}
-generateAndDisplayProducts(0);
-radioInputs.forEach((input, index) => {
-  input.addEventListener('change', () => {
-    generateAndDisplayProducts(index);
-  });
-});
 
-
-
-
-// add to cart button function
-document.querySelectorAll('.js-add-to-cart').forEach(
+  // add to cart button function
+  document.querySelectorAll('.js-add-to-cart').forEach(
   (button) => {
     button.addEventListener('click', () => {
+      
       const productName = button.dataset.productName;
+      const quantitySelector = document.querySelector(`.js-quantity-selector[data-product-name="${productName}"]`);
+      
+      const quantity = Number(quantitySelector.value);
       let matchingItem;
 
       cart.forEach((item) => {
@@ -193,12 +190,13 @@ document.querySelectorAll('.js-add-to-cart').forEach(
         }
       });
 
+      
       if (matchingItem) {
-        matchingItem.quantity += 1;
+        matchingItem.quantity += quantity;
       } else {
         cart.push({
           productName,
-          quantity: 1
+          quantity: quantity
         });
       }
 
@@ -210,4 +208,68 @@ document.querySelectorAll('.js-add-to-cart').forEach(
       document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     });
   });
-  
+}
+
+
+// Ajoutez un écouteur d'événement pour chaque label
+document.querySelectorAll('.cards label').forEach((label, index) => {
+  label.addEventListener('click', () => {
+    // Supprimez la classe 'active' de tous les indicateurs
+    indicators.forEach((indicator) => {
+      indicator.classList.remove('active');
+    });
+
+    // Ajoutez la classe 'active' à l'indicateur actuel
+    indicators[index].classList.add('active');
+    // Sélectionnez le bouton radio correspondant
+    radioInputs[index].checked = true;
+
+    // Générez et affichez les produits pour la catégorie sélectionnée
+    generateAndDisplayProducts(index);
+
+    // Mettez à jour l'indicateur du slider
+    updateSliderIndicator(index);
+  });
+});
+
+// Ajoutez un écouteur d'événement pour chaque indicateur
+indicators.forEach((indicator, index) => {
+  indicator.addEventListener('click', () => {
+    // Supprimez la classe 'active' de tous les indicateurs
+    indicators.forEach((otherIndicator) => {
+      otherIndicator.classList.remove('active');
+    });
+
+    // Ajoutez la classe 'active' à l'indicateur actuel
+    indicator.classList.add('active');
+
+    // Sélectionnez le bouton radio correspondant
+    radioInputs[index].checked = true;
+
+    // Générez et affichez les produits pour la catégorie sélectionnée
+    generateAndDisplayProducts(index);
+
+    // Mettez à jour l'indicateur du slider
+    updateSliderIndicator(index);
+  });
+});
+
+generateAndDisplayProducts(0);
+radioInputs.forEach((input, index) => {
+  input.addEventListener('change', () => {
+    generateAndDisplayProducts(index);
+  });
+});
+
+function updateSliderIndicator(index) {
+  // Retirez la classe active de tous les indicateurs
+  indicators.forEach(indicator => indicator.classList.remove('active'));
+
+  // Ajoutez la classe active à l'indicateur correspondant à la diapositive sélectionnée
+  indicators[index].classList.add('active');
+}
+
+
+
+
+
